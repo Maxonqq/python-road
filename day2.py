c59eab1063a65a36669aede56b4506fb8043ad71
Maxonqq <152058658+Maxonqq@users.noreply.github.com>
2025-10-31 00:09:05 +0300

    Добавил цикл для повторного ввода

--- day2.py
+++ day2.py
@@ -1,1 +1,31 @@
-print("Try change a file")
+def getName():
+    print("Введите имя: ", end="")
+    return input()
+
+def getAge():
+    print("Введите возраст: ", end="")
+    return int(input())
+
+def is_adult(age: int) -> bool:
+    if age >= 18:
+        return True
+    return False
+
+def main():
+
+    while True:
+        name: str = getName()
+
+        if name.lower() == "стоп":
+            print("Выход из программы...")
+            break
+
+        age: int = getAge()
+
+        print(f"Привет, {name}! Тебе {age} лет.")
+        if is_adult(age):
+            print("Доступ разрешен\n")
+        else:
+            print("Доступ запрещен\n")
+
+main()
