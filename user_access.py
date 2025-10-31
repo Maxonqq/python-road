163c0ca0850221fc472e8d1db4e50e57b95c0935
Maxonqq <152058658+Maxonqq@users.noreply.github.com>
2025-11-01 02:22:11 +0300

    Изучил конструкцию try/except и добавил ловлю ошибок

--- user_access.py
+++ user_access.py
@@ -1,10 +1,40 @@
-def getName():
-    print("Введите имя: ", end="")
-    return input()
+class AgeTooBigError(Exception):
+    pass
 
-def getAge():
-    print("Введите возраст: ", end="")
-    return int(input())
+class AgeIsNegativeError(Exception):
+    pass
+
+def getName() -> str:
+    while True:
+        try:
+            name = input("Введите имя: ")
+
+            if not name.isalpha():
+                raise Exception("имя должно состоять только из букв")
+
+            return name
+        except Exception as e:
+            print("Ошибка:", e)
+
+
+def getAge() -> int:
+    while True:
+        try:
+            age = int(input("Введите ваш возраст: "))
+
+            if age > 100:
+                raise AgeTooBigError("возраст не может быть больше 100")
+            if age < 0:
+                raise AgeIsNegativeError("возраст не может быть отрицательным")
+
+            return age
+        
+        except AgeTooBigError as ATBE:
+            print("Ошибка:", ATBE)
+        except AgeIsNegativeError as EINE:
+            print("Ошибка:", EINE)
+        except ValueError:
+            print("Ошибка: возраст должен быть числом")
 
 def is_adult(age: int) -> bool:
     if age >= 18:
